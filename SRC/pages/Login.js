d847dc092c02f083db737d43c67033a016ae1180
nicksaller <93552167+nicksaller@users.noreply.github.com>
2023-09-24 04:26:43 -0300

    Update Login.js

--- SRC/pages/Login.js
+++ SRC/pages/Login.js
@@ -7,6 +7,40 @@ function Login() {
   const handleLogin = () => {
     // Aqui você pode implementar a lógica de login no Firebase Authentication
     // e redirecionar para a página Principal se o login for bem-sucedido.
+    const firebase = require('firebase/app');
+require('firebase/auth');
+
+// Configure o Firebase com as configurações do seu projeto
+const firebaseConfig = {
+  apiKey: 'SUA_API_KEY',
+  authDomain: 'SEU_DOMÍNIO.firebaseapp.com',
+  projectId: 'SEU_PROJETO_ID',
+  storageBucket: 'SEU_BUCKET.appspot.com',
+  messagingSenderId: 'SEU_SENDER_ID',
+  appId: 'SEU_APP_ID',
+};
+
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Função para lidar com o login
+const handleLogin = async (email, senha) => {
+  try {
+    // Faça login com email e senha
+    await firebase.auth().signInWithEmailAndPassword(email, senha);
+
+    // Se o login for bem-sucedido, redirecione para a página Principal
+    window.location.href = 'pagina-principal.html';
+  } catch (error) {
+    console.error('Erro ao fazer login: ', error.message);
+  }
+};
+
+// Exemplo de uso da função
+const email = 'seuemail@example.com';
+const senha = 'suasenha';
+
+handleLogin(email, senha);
   };
 
   return (
