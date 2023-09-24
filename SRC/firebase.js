789ea7bd6aa2362501d6351849fdcbadf8a3370f
nicksaller <93552167+nicksaller@users.noreply.github.com>
2023-09-24 15:03:21 -0300

    Update firebase.js

--- SRC/firebase.js
+++ SRC/firebase.js
@@ -2,13 +2,23 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'VUz1Ia5uhlImr5qJXCGu',
-  authDomain: 'https://console.firebase.google.com/project/nicksaller/firestore/data/~2Fusuarios',
-  projectId: 'NickSaller',
-  storageBucket: 'SEU_STORAGE_BUCKET',
-  messagingSenderId: 'SEU_MESSAGING_SENDER_ID',
-  appId: 'SEU_APP_ID',
+  apiKey: "AIzaSyD059YmR2bWpIt_eM_E8kQzMATPdCtRNtY",
+  authDomain: "projetotdw-f3e61.firebaseapp.com",
+  projectId: "projetotdw-f3e61",
+  storageBucket: "projetotdw-f3e61.appspot.com",
+  messagingSenderId: "285916136748",
+  appId: "1:285916136748:web:cc09915ff5699dd36d9889"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 };
 
 firebase.initializeApp(firebaseConfig);
